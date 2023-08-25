15e58adb0ca9996d034414c1f4b9067938c03207
JessicaBane <jessicambane@gmail.com>
2023-08-25 10:01:24 +0100

    updated

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -27,9 +27,9 @@ export default function ProjectPage({params}:ProjectPageParams) {
 
   return (
     <main className="p-3">
-      <h2 className={`${merienda.className} text-3xl p-2`}>{project.title}</h2>
-      <Image className="mx-auto p-2 h-60 w-fit" placeholder="blur" blurDataURL={`/_next/image?url=${project?.images}&w=16&q=1`} src={`${project?.images}`} alt={`${project?.title}`} width={200} height={200}/>     
-      <p className="text-center p-2">{project.description}</p>
+      <h2 className={`${merienda.className} text-green-400 text-4xl p-2 text-center`}>{project.title}</h2>
+      <Image className="mx-auto p-2 h-80 w-fit" placeholder="blur" blurDataURL={`/_next/image?url=${project?.images}&w=16&q=1`} src={`${project?.images}`} alt={`${project?.title}`} width={200} height={200}/>     
+      <p className="text-center p-2 text-xl">{project.description}</p>
       <p className="text-right"> Click <Link className={`${merienda.className} text-xl  text-green-500 hover:text-blue-500`} href={project.link}>HERE</Link> for the site and pattern.</p>
       <div dangerouslySetInnerHTML={{ __html: project.body.html }} className="prose-h3:text-blue-500 prose-p:m-2 prose-img:w-3/4 prose-img:p-3 prose-img:m-auto dark:prose-invert p-2 prose-ul:grid prose-ul:grid-cols-1 md:prose-ul:grid-cols-2 "></div> 
       <Comments slug={params.slug}/>
